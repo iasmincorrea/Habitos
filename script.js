bd49ecc764d77ccbc442b8fffaa1ed57b1685a3b
iasmincorrea <iasmin_correa@hotmail.com>
2023-01-20 16:12:06 -0300

    Javascript

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
-const form = document.querySelector('#form-habits')
+const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
 
-const data = {
-    food: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists) {
+
+        return
+    } 
+
+    nlwSetup.addDay(today)
 }
 
+function save() {
+    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
+
+// const data = {
+//     food: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
+// }
